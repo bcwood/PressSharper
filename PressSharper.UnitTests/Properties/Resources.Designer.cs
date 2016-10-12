817d563ff029945175ca08873c69d8c22dee12d4
Brandon Wood <bcwood@uaa.alaska.edu>
2016-10-12 14:14:39 -0800

    updated .net version to 4.5.2

--- PressSharper.UnitTests/Properties/Resources.Designer.cs
+++ PressSharper.UnitTests/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PressSharperer.UnitTests.Properties {
+namespace PressSharper.UnitTests.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace PressSharperer.UnitTests.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PressSharperer.UnitTests.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PressSharper.UnitTests.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
